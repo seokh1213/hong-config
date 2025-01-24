399110e159c1e01512781245a9ca1dd14b9378fe
홍석현 <seokh1213@gmail.com>
2025-01-24 12:09:39 +0900

    visual copy 추가

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -19,3 +19,33 @@ mapKey('<leader>h', ':nohlsearch<CR>')
 -- indent
 mapKey('<', '<gv', 'v')
 mapKey('>', '>gv', 'v')
+
+-- copy
+vim.api.nvim_create_autocmd("TextYankPost", {
+	pattern = "*",
+	callback = function()
+		if vim.v.event.operator == "y" then
+			local start_line = vim.fn.line("'<")
+			local end_line = vim.fn.line("'>")
+			local start_col = vim.fn.col("'<")
+			local end_col = vim.fn.col("'>")
+
+			local lines = vim.fn.getline(start_line, end_line)
+
+			if #lines > 0 then
+				-- Handle first line
+				if start_col > 1 then
+					lines[1] = lines[1]:sub(start_col)
+				end
+
+				-- Handle last line
+				if end_col < #lines[#lines] then
+					lines[#lines] = lines[#lines]:sub(1, end_col)
+				end
+
+				local yank_text = table.concat(lines, "\n")
+				vim.fn.setreg("+", yank_text)
+			end
+		end
+	end,
+})
