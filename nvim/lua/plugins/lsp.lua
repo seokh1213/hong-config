23e751e71c605f74fe757d8b3c61d38ceacdcba9
홍석현 <seokh1213@gmail.com>
2025-01-24 10:28:34 +0900

    nvim 수정

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,32 +1,44 @@
-local keyMapper = require('utils.keyMapper').mapKey
+local keyMapper = require("utils.keyMapper").mapKey
 
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require('mason').setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require('mason-lspconfig').setup({
-        ensure_installed = { "lua_ls", "ts_ls", "java_language_server", "kotlin_language_server" }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
-      lspconfig.java_language_server.setup({})
-      lspconfig.kotlin_language_server.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					"java_language_server",
+					"kotlin_language_server",
+					"isort",
+					"black",
+					"ast-grep",
+					"ktfmt",
+					"prettier",
+					"prettierd",
+					"stylua",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.ts_ls.setup({})
+			lspconfig.java_language_server.setup({})
+			lspconfig.kotlin_language_server.setup({})
 
-      keyMapper('K', vim.lsp.buf.hover)
-      keyMapper('gd', vim.lsp.buf.definition)
-      keyMapper('<leader>ca', vim.lsp.buf.code_action)
-    end
-  }
+			keyMapper("K", vim.lsp.buf.hover)
+			keyMapper("gd", vim.lsp.buf.definition)
+			keyMapper("<leader>ca", vim.lsp.buf.code_action)
+		end,
+	},
 }
